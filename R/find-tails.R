54f709f1b0cd28e5eb01a5e0b3de3b66aabadf67
miraclezero <flmajia@gmail.com>
2024-09-28 20:54:42 -0500

    change makecluster

--- R/find-tails.R
+++ R/find-tails.R
@@ -312,8 +312,8 @@ find_tails <- function(fast5_dir,
 
     # Make a compute cluster
     cat(paste(cli::symbol$bullet,' Starting a parallel compute cluster...\n', sep=''))
-    #cl <- parallel::makeCluster(num_cores, outfile='')
-    cl <- parallel::makeCluster(num_cores)
+    cl <- parallel::makeCluster(num_cores, outfile='')
+    #cl <- parallel::makeCluster(num_cores)
     on.exit(parallel::stopCluster(cl))
 
     doSNOW::registerDoSNOW(cl)
